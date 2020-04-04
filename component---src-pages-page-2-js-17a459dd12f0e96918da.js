fac221638a7f75ef096c7b2200a0afa3e3d20d90
luciaherrrador <luciaherradorcarrasco@gmail.com>
2020-04-04 23:03:31 +0200

    Updates

---
+++ component---src-pages-page-2-js-17a459dd12f0e96918da.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{p5nM:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),c=a("TJpk"),o=a.n(c),r=a("7oih"),p=a("VUD3");t.default=function(){return l.a.createElement(r.a,{pageName:"two"},l.a.createElement(o.a,null,l.a.createElement("title",null,"Page Two")),l.a.createElement(p.a,{type:"content",className:"text-center"},l.a.createElement("h1",null,"Page Two"),l.a.createElement("p",null,"Welcome to page 2")))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-17a459dd12f0e96918da.js.map
