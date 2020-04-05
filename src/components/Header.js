607dd44d4b33e10d3fd3933eb3cdf81f5dfa4f95
luciaherrrador <luciaherradorcarrasco@gmail.com>
2020-04-05 15:49:34 +0200

    styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header>
       <Container type="content">
-        <p>My Gatsby Site</p>
+        <p>CoronaMap</p>
         <ul>
           <li>
             <Link to="/">Home</Link>
